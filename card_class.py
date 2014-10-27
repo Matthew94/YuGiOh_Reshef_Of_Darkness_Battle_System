f131d6bc2b6930f68afcab4a411b72a4eccfa8ca
Matthew Love <matthewlove94@gmail.com>
2014-10-27 02:58:05 +0000

    subdued skeleton classes

--- card_class.py
+++ card_class.py
@@ -1,51 +1,43 @@
 class CardBase(object):
-    def __init__(self, title = "", description = "", card_number = 0):
+    def __init__(self, title = "", description = "", card_number = 0,
+                 type = ""):
         """Base class for Card. Just has name and description."""
         self.title = title
         self.description = description
         self.card_number = card_number
-        
-class NormalMonsterCard(CardBase):
-    def __init__(self, title = "", description = "", card_number = 0,
-                level = 1, attack = 0, defence = 0, attribute = "", 
-                type = ""):
-        super(NormalMonsterCard, self).__init__(title, description, 
-                                                card_number)
-        
-        self.level = level
-        
-        self.attack = attack
-        self.defence = defence
-        
-        self.attribute = attribute
         self.type = type
 
-def EffectMonsterCard(NormalMonsterCard):
+class NormalMonsterCard(CardBase):
     def __init__(self, title = "", description = "", card_number = 0,
-                level = 1, attack = 0, defence = 0, attribute = "", 
-                type = ""):
-        super(NormalMonsterCard, self).__init__(title, description, 
-                                                card_number, level, attack,
-                                                defence, attribute, type)
+                 type = "", level = 1, attack = 0, defence = 0,
+                 attribute = "", element = ""):
+        super(self).__init__(title, description, card_number, type)
 
-def FusionMonsterCard(object):
+        self.level = level
+
+        self.attack = attack
+        self.defence = defence
+
+        self.attribute = attribute
+        self.element = element
+
+class EffectMonsterCard(object):
     pass
-    
-def RitualMonsterCard(object):
+
+class FusionMonsterCard(object):
+    pass
+
+class RitualMonsterCard(object):
     pass
 
 class MagicCard(CardBase):
-    def __init__(self, title = "", description = "", card_number = 0):
-        super(NormalMonsterCard, self).__init__(title, description, 
-                                                card_number)
-                                                
+    pass
+
 class FieldMagicCard(object):
     pass
 
 class TrapCard(CardBase):
-    def __init__(self, title = "", description = "", card_number = 0):
-        super(NormalMonsterCard, self).__init__(title, description, 
-                                                card_number)
-                                                
+    pass
+
 class TokenCard(object):
     pass
